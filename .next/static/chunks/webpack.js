396e0451f3003d25e2ab0904e90fcd8f84694395
ArmandoKatito10 <armandokatito10@gmail.com>
2022-12-23 09:59:24 +0100

    adding all swipers

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e17640264e4d8fb"; }
+/******/ 		__webpack_require__.h = function() { return "f03032c754e6b3bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
